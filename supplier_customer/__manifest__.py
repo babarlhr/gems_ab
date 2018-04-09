75780847f09423550cfdce95f0435ca5ed654bb8
Muhammad Younis <younis@odoo.ie@gmail.com>
2018-04-09 15:25:26 +0500

    Separated customer and supplier menu from contacts and hide contacts menu.

---
+++ supplier_customer/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Customer and Supplier",
+    "version": "1.0",
+    "author": "Odoo Advantage Ireland",
+    "website": "www.odoo.ie",
+    "category": "contacts",
+    "depends": ['contacts', 'base'],
+    "data": [
+        'views/contacts_menu.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}

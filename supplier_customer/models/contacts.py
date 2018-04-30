787f30543e8279316c9f1436be617f36342c905c
Muhammad Younis <younis@odoo.ie@gmail.com>
2018-04-30 10:26:56 +0500

    [Customer Default value change to false]

--- supplier_customer/models/contacts.py
+++ supplier_customer/models/contacts.py
@@ -5,6 +5,8 @@ class ResPartner(models.Model):
     
     contact_cr = fields.Char('CR')
     regions_tag = fields.Many2many('res.partner.regions', string="Tags (Region)")
+    customer = fields.Boolean(string='Is a Customer', default=False,
+                               help="Check this box if this contact is a customer.")
     
 class ResPartnerRegions(models.Model):
     _name = 'res.partner.regions'

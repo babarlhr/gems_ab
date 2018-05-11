e09cbb7cdb1ba62ddaeb22775a0d57bf3cf7b343
Muhammad Younis <younis@odoo.ie@gmail.com>
2018-05-11 11:00:46 +0500

    Error Fixed

--- procurement_custom/__manifest__.py
+++ procurement_custom/__manifest__.py
@@ -3,8 +3,8 @@
     'author' : 'Odoo Advantage Ireland',
     'website' : 'erp.odoo.ie',
     'data' : [
-            'views/procurement.xml',
             'security/security.xml',
+            'views/procurement.xml',
         ],
     'depends' : ['purchase','base','project','capex_procurement'],
     'installable' : True,

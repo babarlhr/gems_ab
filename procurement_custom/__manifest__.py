61c5ba29cd1c4acb98e284f09bf0ee71c0a79bd5
Muhammad Younis <younis@odoo.ie@gmail.com>
2018-05-11 10:56:41 +0500

    Add/Remove fields

--- procurement_custom/__manifest__.py
+++ procurement_custom/__manifest__.py
@@ -4,6 +4,7 @@
     'website' : 'erp.odoo.ie',
     'data' : [
             'views/procurement.xml',
+            'security/security.xml',
         ],
     'depends' : ['purchase','base','project','capex_procurement'],
     'installable' : True,

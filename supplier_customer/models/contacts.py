c8e8f5b3f885208ab5158dc7c65040fe3f7891b3
Muhammad Younis <younis@odoo.ie@gmail.com>
2018-05-15 21:59:55 +0500

    [Billing and shipping address view customization]

--- supplier_customer/models/contacts.py
+++ supplier_customer/models/contacts.py
@@ -7,6 +7,10 @@ class ResPartner(models.Model):
     regions_tag = fields.Many2many('res.partner.regions', string="Tags (Region)")
     customer = fields.Boolean(string='Is a Customer', default=False,
                                help="Check this box if this contact is a customer.")
+    longitude = fields.Char('Longitude')
+    latitude = fields.Char('Latitude')
+    google_address = fields.Char('Google Map Address')
+    street3 = fields.Char('Address')
     
 class ResPartnerRegions(models.Model):
     _name = 'res.partner.regions'
